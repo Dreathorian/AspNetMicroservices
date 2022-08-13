cf73828cd5786512bef49f2a6cf537d068af7602
ChristophWolf1 <christoph.wolf1@softwareone.com>
2022-08-13 21:37:11 +0200

    Added Catalog

---
+++ AspNetMicroservices/src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver.Linq;
+
+namespace Catalog.API.Repositories;
+
+public interface IProductRepository
+{
+    IMongoQueryable<Product> GetAll();
+
+    Task<Product> GetById(string id);
+
+    IMongoQueryable<Product> GetByName(string name);
+
+    IMongoQueryable<Product> GetByCategory(string cateogryName);
+
+    Task CreateProduct(Product product);
+
+    Task<bool> UpdateProduct(Product product);
+
+    Task<bool> DeleteProduct(string id);
+}

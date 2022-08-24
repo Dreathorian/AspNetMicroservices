d66983fc195d6083c7210b8e1a4903c16556e015
ChristophWolf1 <christoph.wolf1@softwareone.com>
2022-08-24 11:04:18 +0200

    apply gitignore

--- AspNetMicroservices/src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-
-namespace Catalog.API.Entities;
-
-public interface ICatalogContext
-{
-    IMongoCollection<Product> Products { get; }  
-}
